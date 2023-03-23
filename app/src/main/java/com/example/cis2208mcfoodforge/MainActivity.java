5f062f6587eb7fa2b8805fcb730ef25498321797
David Briffa <david.briffa.21@um.edu.mt>
2023-03-23 17:54:16 +0100

    colours

--- app/src/main/java/com/example/cis2208mcfoodforge/MainActivity.java
+++ app/src/main/java/com/example/cis2208mcfoodforge/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        getWindow().setNavigationBarColor(getColor(R.color.orange_200));
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
