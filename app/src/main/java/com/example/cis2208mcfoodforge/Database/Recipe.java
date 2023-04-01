626ac5826633b11997d2fd2079a122c7d711fc5e
David Briffa <david.briffa.21@um.edu.mt>
2023-04-01 15:22:55 +0200

    recycler views for discovery

--- app/src/main/java/com/example/cis2208mcfoodforge/Database/Recipe.java
+++ app/src/main/java/com/example/cis2208mcfoodforge/Database/Recipe.java
@@ -3,9 +3,11 @@ package com.example.cis2208mcfoodforge.Database;
 public class Recipe {
     private String recipe_description;
     private String recipe_name;
-    private String favourite_count;
+    private int favourite_count;
     private int user_id;
     private int recipe_id;
+    private int difficulty;
+
 
     public String getRecipe_description() {
         return recipe_description;
@@ -15,7 +17,7 @@ public class Recipe {
         return recipe_name;
     }
 
-    public String getFavourite_count() {
+    public int getFavourite_count() {
         return favourite_count;
     }
 
@@ -26,8 +28,10 @@ public class Recipe {
     public int getRecipe_id() {
         return recipe_id;
     }
+    public int getDifficulty() { return difficulty; }
 
-    public Recipe(String recipe_description, String recipe_name, String favourite_count, int user_id, int recipe_id) {
+    public Recipe(String recipe_description, String recipe_name, int favourite_count, int user_id, int recipe_id, int difficulty) {
+        this.difficulty = difficulty;
         this.recipe_description = recipe_description;
         this.recipe_name = recipe_name;
         this.favourite_count = favourite_count;
--- app/src/main/java/com/example/cis2208mcfoodforge/ui/discover/DiscoverFragment.java
+++ app/src/main/java/com/example/cis2208mcfoodforge/ui/discover/DiscoverFragment.java
@@ -17,29 +17,145 @@ import com.example.cis2208mcfoodforge.Database.Recipe;
 import com.example.cis2208mcfoodforge.R;
 import com.example.cis2208mcfoodforge.databinding.FragmentDiscoverBinding;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
 public class DiscoverFragment extends Fragment {
 
     private RecyclerView dailyDishesRecycler;
-    private DiscoverAdapter discoverAdapter;
+    private RecyclerView mostFavouritedRecycler;
+    private RecyclerView easiestRecipesRecycler;
+
+    private DiscoverAdapter mostFavouritedAdapter;
+    private DiscoverAdapter easiestRecipesAdapter;
+
+    private DiscoverAdapter dailyDishesAdapter;
     private List<Recipe> recipes;
+    private List<Recipe> temp;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_discover, container, false);
 
         dailyDishesRecycler = view.findViewById(R.id.dailyDishesRecyclerView);
+        mostFavouritedRecycler = view.findViewById(R.id.mostFavouritedRecyclerView);
+        easiestRecipesRecycler = view.findViewById(R.id.easiestRecipesRecyclerView);
+
         dailyDishesRecycler.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        mostFavouritedRecycler.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        easiestRecipesRecycler.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
+        //reading Json DB data
+        recipes = Arrays.asList(JsonReader.convertJsonToRecipe(requireContext()));
+        temp = recipes;
+
+        //10 random recipes for the daily dishes recycler view
+        Collections.shuffle(recipes);
+        List<Recipe> dailyRecipes = recipes.subList(0, 10);
+
+        temp.sort(Comparator.comparingInt(Recipe::getFavourite_count).reversed());
+        List<Recipe> mostFavourited = temp.subList(0, Math.min(10, temp.size()));
+
+        List<Recipe> easiestRecipes = new ArrayList<>();
+        for(Recipe recipe : recipes){
+            if(recipe.getDifficulty() == 1){
+                easiestRecipes.add(recipe);
+                System.out.println(easiestRecipes.get(0).getRecipe_name());
+            }
+        }
+
+        dailyDishesAdapter = new DiscoverAdapter(dailyRecipes);
+        mostFavouritedAdapter = new DiscoverAdapter(mostFavourited);
+        easiestRecipesAdapter = new DiscoverAdapter(easiestRecipes);
+
+        dailyDishesRecycler.setAdapter(dailyDishesAdapter);
+        mostFavouritedRecycler.setAdapter(mostFavouritedAdapter);
+        easiestRecipesRecycler.setAdapter(easiestRecipesAdapter);
+
+
+        return view;
+    }
+
+}
+ /*
+package com.example.cis2208mcfoodforge.ui.discover;
+
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.TextView;
+
+        import androidx.annotation.NonNull;
+        import androidx.fragment.app.Fragment;
+        import androidx.lifecycle.ViewModelProvider;
+        import androidx.recyclerview.widget.LinearLayoutManager;
+        import androidx.recyclerview.widget.RecyclerView;
+
+        import com.example.cis2208mcfoodforge.Database.JsonReader;
+        import com.example.cis2208mcfoodforge.Database.Recipe;
+        import com.example.cis2208mcfoodforge.R;
+        import com.example.cis2208mcfoodforge.databinding.FragmentDiscoverBinding;
+
+        import java.util.Arrays;
+        import java.util.Comparator;
+        import java.util.List;
+        import java.util.Objects;
+
+
+public class DiscoverFragment extends Fragment {
+
+    private RecyclerView dailyDishesRecycler;
+    private RecyclerView mostFavouritedRecycler;
+    private RecyclerView easiestRecipesRecycler;
+
+    private DiscoverAdapter mostFavouritedAdapter;
+    private DiscoverAdapter easiestRecipesAdapter;
+
+    private DiscoverAdapter dailyDishesAdapter;
+    private List<Recipe> recipes;
+    private List<Recipe> dailyDishes;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_discover, container, false);
+
+        dailyDishesRecycler = view.findViewById(R.id.dailyDishesRecyclerView);
+        //mostFavouritedRecycler = view.findViewById(R.id.mostFavouritedRecyclerView);
+        //easiestRecipesRecycler = view.findViewById(R.id.easiestRecipesRecyclerView);
+
+        dailyDishesRecycler.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        //mostFavouritedRecycler.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        //dailyDishesRecycler.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
+
         recipes = Arrays.asList(JsonReader.convertJsonToRecipe(requireContext()));
 
-        discoverAdapter = new DiscoverAdapter(recipes);
-        dailyDishesRecycler.setAdapter(discoverAdapter);
+        for(Recipe recipe : recipes)
+            if(recipe.getRecipe_id()%3 == 0){
+                dailyDishes.add(recipe);
+            }
+
+
+
+        dailyDishesAdapter = new DiscoverAdapter(dailyDishes);
+        //mostFavouritedAdapter = new DiscoverAdapter(recipes);
+        //easiestRecipesAdapter = new DiscoverAdapter(recipes);
+
+        dailyDishesRecycler.setAdapter(dailyDishesAdapter);
+        //mostFavouritedRecycler.setAdapter(dailyDishesAdapter);
+        // easiestRecipesRecycler.setAdapter(dailyDishesAdapter);
 
         return view;
     }
 
 
 }
+
+  */
