cf91e6496340fea17c8e9c46f4b863840ab2f972
David Briffa <david.briffa.21@um.edu.mt>
2023-04-05 14:24:36 +0200

    orientation- needs work, discovery buggy

--- app/src/main/java/com/example/cis2208mcfoodforge/ui/discover/DiscoverAdapter.java
+++ app/src/main/java/com/example/cis2208mcfoodforge/ui/discover/DiscoverAdapter.java
@@ -38,104 +38,102 @@ public class DiscoverAdapter extends RecyclerView.Adapter<DiscoverAdapter.Recipe
 
         }
 
-        public void MapImages(HashMap<Integer, String> hashmap){
-            hashmap.put(1, "raw/Images/BeefLasagna.jpg");
-            hashmap.put(2, "raw/Images/ChickenPasta.jpg");
-            hashmap.put(3, "raw/Images/RibeyeMushroom.jpg");
-            hashmap.put(4, "raw/Images/StuffedPork.jpg");
-            hashmap.put(5, "raw/Images/Salmon.webp");
-            hashmap.put(6, "raw/Images/Parmigiana.webp");
-            hashmap.put(7, "raw/Images/CurriedRice.jpg");
-            hashmap.put(8, "raw/Images/ChickenThighs.jpg");
-            hashmap.put(9, "raw/Images/PeanutBars.jpg");
-            hashmap.put(10, "raw/Images/CauliflowerChorizo.jpg");
-            hashmap.put(11, "raw/Images/BroccoliGratin.jpg");
-            hashmap.put(12, "raw/Images/BeefGoulash.jpg");
-            hashmap.put(13, "raw/Images/PotatoSalad.jpg");
-            hashmap.put(14, "raw/Images/CaesarSalad.webp");
-            hashmap.put(15, "raw/Images/PorkChops.jpg");
-            hashmap.put(16, "raw/Images/BeefBrisket.webp");
-            hashmap.put(17, "raw/Images/FarfalleSalmon.webp");
-            hashmap.put(18, "raw/Images/BeefSalad.jpg");
-            hashmap.put(19, "raw/Images/ChickenRice.jpg");
-            hashmap.put(20, "raw/Images/SpagBol.webp");
-            hashmap.put(21, "raw/Images/CrustyBread.jpg");
-            hashmap.put(22, "raw/Images/BaconOnionOmelette.jpg");
-            hashmap.put(23, "raw/Images/mushroomomelette.webp");
-            hashmap.put(24, "raw/Images/Couscous.jpg");
-            hashmap.put(25, "raw/Images/CrispyPotatoes.jpg");
-            hashmap.put(26, "raw/Images/PenneNorma.jpg");
-            hashmap.put(27, "raw/Images/BeefTacos.jpg");
-            hashmap.put(28, "raw/Images/MashedPotatoes.webp");
-            hashmap.put(29, "raw/Images/MushroomSauce.jpg");
-            hashmap.put(30, "raw/Images/PizzaDough.webp");
-            hashmap.put(31, "raw/Images/chickenNuggets.jpg");
+    public void MapImages(HashMap<Integer, String> hashmap){
+        hashmap.put(1, "raw/Images/BeefLasagna.jpg");
+        hashmap.put(2, "raw/Images/ChickenPasta.jpg");
+        hashmap.put(3, "raw/Images/RibeyeMushroom.jpg");
+        hashmap.put(4, "raw/Images/StuffedPork.jpg");
+        hashmap.put(5, "raw/Images/Salmon.webp");
+        hashmap.put(6, "raw/Images/Parmigiana.webp");
+        hashmap.put(7, "raw/Images/CurriedRice.jpg");
+        hashmap.put(8, "raw/Images/ChickenThighs.jpg");
+        hashmap.put(9, "raw/Images/PeanutBars.jpg");
+        hashmap.put(10, "raw/Images/CauliflowerChorizo.jpg");
+        hashmap.put(11, "raw/Images/BroccoliGratin.jpg");
+        hashmap.put(12, "raw/Images/BeefGoulash.jpg");
+        hashmap.put(13, "raw/Images/PotatoSalad.jpg");
+        hashmap.put(14, "raw/Images/CaesarSalad.webp");
+        hashmap.put(15, "raw/Images/PorkChops.jpg");
+        hashmap.put(16, "raw/Images/BeefBrisket.webp");
+        hashmap.put(17, "raw/Images/FarfalleSalmon.webp");
+        hashmap.put(18, "raw/Images/BeefSalad.jpg");
+        hashmap.put(19, "raw/Images/ChickenRice.jpg");
+        hashmap.put(20, "raw/Images/SpagBol.webp");
+        hashmap.put(21, "raw/Images/CrustyBread.jpg");
+        hashmap.put(22, "raw/Images/BaconOnionOmelette.jpg");
+        hashmap.put(23, "raw/Images/mushroomomelette.webp");
+        hashmap.put(24, "raw/Images/Couscous.jpg");
+        hashmap.put(25, "raw/Images/CrispyPotatoes.jpg");
+        hashmap.put(26, "raw/Images/PenneNorma.jpg");
+        hashmap.put(27, "raw/Images/BeefTacos.jpg");
+        hashmap.put(28, "raw/Images/MashedPotatoes.webp");
+        hashmap.put(29, "raw/Images/MushroomSauce.jpg");
+        hashmap.put(30, "raw/Images/PizzaDough.webp");
+        hashmap.put(31, "raw/Images/chickenNuggets.jpg");
 
+    }
+
+    @NonNull
+    @Override
+    public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recipe_list_item, parent, false);
+        return new RecipeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        Recipe recipe = recipes.get(position);
+        String imageFilename = imageHashMap.get(recipe.getRecipe_id());
+        RequestOptions requestOptions = new RequestOptions();
+
+        holder.bind(recipe.getRecipe_name());
+
+        Glide.with(context)
+                .load("file:///android_asset/" + imageFilename)
+                .apply(requestOptions
+                        .transforms(new CenterCrop(), new RoundedCorners(40))
+                        .override(600))
+                .into(holder.recipeImageView);
+        holder.recipeImageView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Recipe selectedRecipe = recipes.get(position);
+                Intent intent = new Intent(context, RecipeDetailsActivity.class);
+
+                intent.putExtra("recipeName", selectedRecipe.getRecipe_name());
+                intent.putExtra("description", selectedRecipe.getRecipe_description());
+                intent.putExtra("favouriteCount", selectedRecipe.getFavourite_count());
+                intent.putExtra("id", selectedRecipe.getRecipe_id());
+                intent.putExtra("difficulty", selectedRecipe.getDifficulty());
+                intent.putExtra("author", selectedRecipe.getUser_id());
+
+                // Launch the new activity
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipes.size();
+    }
+
+    public static class RecipeViewHolder extends RecyclerView.ViewHolder {
+        public TextView recipeNameTextView;
+        public ImageView recipeImageView;
+
+        public RecipeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            recipeNameTextView = itemView.findViewById(R.id.recipeNameTextView);
+            recipeImageView = itemView.findViewById(R.id.recipeImageView);
         }
 
-        @NonNull
-        @Override
-        public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.recipe_list_item, parent, false);
-            return new RecipeViewHolder(view);
+        public void bind(String recipeName) {
+            recipeNameTextView.setText(recipeName);
         }
 
-        @Override
-        public void onBindViewHolder(@NonNull RecipeViewHolder holder, @SuppressLint("RecyclerView") int position) {
-            Recipe recipe = recipes.get(position);
-            String imageFilename = imageHashMap.get(recipe.getRecipe_id());
-            RequestOptions requestOptions = new RequestOptions();
-
-            holder.bind(recipe.getRecipe_name());
-
-            Glide.with(context)
-                    .load("file:///android_asset/" + imageFilename)
-                    .apply(requestOptions
-                            .transforms(new CenterCrop(), new RoundedCorners(40))
-                            .override(600))
-                    .into(holder.recipeImageView);
-            holder.recipeImageView.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View view) {
-                    Recipe selectedRecipe = recipes.get(position);
-                    Intent intent = new Intent(context, RecipeDetailsActivity.class);
-
-                    intent.putExtra("recipeName", selectedRecipe.getRecipe_name());
-                    intent.putExtra("description", selectedRecipe.getRecipe_description());
-                    intent.putExtra("favouriteCount", selectedRecipe.getFavourite_count());
-                    intent.putExtra("id", selectedRecipe.getRecipe_id());
-                    intent.putExtra("difficulty", selectedRecipe.getDifficulty());
-                    intent.putExtra("author", selectedRecipe.getUser_id());
-
-                    // Launch the new activity
-                    context.startActivity(intent);
-                }
-            });
-            }
-
-
-        @Override
-        public int getItemCount() {
-            return recipes.size();
-        }
-
-        public static class RecipeViewHolder extends RecyclerView.ViewHolder {
-
-            public TextView recipeNameTextView;
-            public ImageView recipeImageView;
-
-            public RecipeViewHolder(@NonNull View itemView) {
-                super(itemView);
-                recipeNameTextView = itemView.findViewById(R.id.recipeNameTextView);
-                recipeImageView = itemView.findViewById(R.id.recipeImageView);
-
-            }
-
-            public void bind(String recipeName) {
-                recipeNameTextView.setText(recipeName);
-            }
         }
     }
 
