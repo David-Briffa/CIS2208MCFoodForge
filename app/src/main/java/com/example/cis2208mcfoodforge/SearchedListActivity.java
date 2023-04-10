45d3f78e022a5575d3399c5624212d209e1c581b
David Briffa <david.briffa.21@um.edu.mt>
2023-04-10 15:40:09 +0200

    back button for search activity

--- app/src/main/java/com/example/cis2208mcfoodforge/SearchedListActivity.java
+++ app/src/main/java/com/example/cis2208mcfoodforge/SearchedListActivity.java
@@ -5,11 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -35,6 +39,19 @@ public class SearchedListActivity extends AppCompatActivity {
         RecyclerView searchedListRecyclerView = findViewById(R.id.searchedListRecyclerView);
         searchedListRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
+        //back button to return to discover
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
         Intent intent = getIntent();
         Recipe[] recipes = JsonReader.convertJsonToRecipe(this);
 
@@ -52,14 +69,20 @@ public class SearchedListActivity extends AppCompatActivity {
         } else {
             System.out.println("False");
         }
-
-
         SearchListAdapter adapter = new SearchListAdapter(searchResults, this);
         searchedListRecyclerView.setAdapter(adapter);
-
-
-
     }
+
+    @Override
+    public void onBackPressed() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (fragmentManager.getBackStackEntryCount() > 1) {
+            fragmentManager.popBackStack();
+        } else {
+            super.onBackPressed();
+        }
     }
+}
 
 
